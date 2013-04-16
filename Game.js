97953900fb348f94325ab54be5870cf3b1e14a06
Andy <andy@thatguynamedandy.com>
2013-04-16 17:35:02 +0100

    Clean up of Game code

---
+++ Game.js
@@ -0,0 +1,115 @@
+var _ = require('underscore'),
+	Backbone = require('backbone'),
+	CollisionDetection = require('./utils/CollisionDetection'),
+	TankModel = require('./model/TankModel'),
+	BoundsModel = require('./model/BoundsModel'),
+	ElementCollection = require('./collection/ElementCollection'),
+	define = require('amdefine')(module);
+
+define(function(require) {
+		
+	var FRAME_RATE = 20;
+		
+	return {
+	
+		frame: function(){
+	
+			//trigger frame advance
+			this.events.trigger("frame:advance");
+			
+			//look for collisions between objects
+			_.each(this.collection.models, function(model, idx, collection) {
+				if(typeof model !== "undefined") { 	
+					CollisionDetection.detect(model, collection);
+				}
+			});		
+			
+			//look for collision with bounds
+			CollisionDetection.detect(this.boundsModel, this.collection.models, {invert:true});
+			
+			//emit output to each socket
+			_.each(this.sockets, function(socket){
+				socket.emit('frame', this.collection.toJSON());	
+			}, this);
+			
+		},
+	
+		addSocket: function(socket) {
+
+			//horrible context reference FIXME;
+			var self = this;
+
+			this.sockets.push(socket);
+		
+			//spawn tank in random location
+			//TODO: ensure the location is empty
+			this.collection.add(
+				new TankModel({
+					'id': socket.id,
+					'type': 'tank',
+					'x': _.random(0, this.boundsModel.get('x')),
+					'y': _.random(0, this.boundsModel.get('y')),
+					'a': _.random(0, 360)
+				}, {
+					'events': this.events
+				}
+			));
+
+			//set up user controls			
+			socket.on('move', function(move) {
+				try {
+					self.collection.get(socket.id).set('move', move);
+				} catch(e) {}
+			});
+		
+			socket.on('rotate', function(rotate) {
+				try {
+					self.collection.get(socket.id).set('rotate', rotate);
+				} catch(e) {}
+			});
+		
+			socket.on('shoot', function(shoot) {
+				try {
+					self.collection.get(socket.id).shoot();
+				} catch(e) {}
+			});
+		
+			socket.on('disconnect', function() {
+				self.collection.remove(self.collection.get(socket.id));
+			});
+		},
+	
+		start: function(opts) {
+			
+			//horrid context hack FIXME;
+			var self = this;
+			
+			this.io = opts.io;
+			
+			//master collection of all game elements
+			this.collection = new ElementCollection();
+
+			//bounds model determines game edges
+			this.boundsModel = new BoundsModel();
+		
+			//events object handles frame advance
+			this.events = _.extend({}, Backbone.Events);
+
+			//sockets array
+			this.sockets = [];
+						
+			//when a new socket is opened call add socket
+			this.io.sockets.on('connection', function(){
+				self.addSocket.apply(self, arguments);
+			});
+			
+			//start game interval
+			frameInterval = setInterval(function(){
+				self.frame.apply(self);
+			}, FRAME_RATE);
+			
+			console.log('====**COMMENCE WAR**=====');
+		}
+	
+	}
+});
--- app.js
+++ app.js
@@ -1,93 +1,14 @@
-//TODO: This is all currently very crude
-
 var express = require('express'),
 	http = require('http'),
-	Backbone = require('backbone'),
-	_ = require('underscore'),
-	CollisionDetection = require('./utils/CollisionDetection'),
-	TankModel = require('./model/TankModel'),
-	BoundsModel = require('./model/BoundsModel'),
-	ElementCollection = require('./collection/ElementCollection'),
+	Game = require('./Game'),
 	app = express(),
 	server = http.createServer(app),
 	io = require('socket.io').listen(server, { log: false });
-
-//constants
-var FRAME_RATE = 20;
-
-var elementCollection = new ElementCollection(),
-	boundsModel = new BoundsModel(),
-	events = _.extend({}, Backbone.Events),
-	sockets = [];
-
 server.listen(8080);
 app.use(express.static(__dirname + '/'));
-//app.use('node', express.static(__dirname + '/node_modules'));
-
-console.log('====TANKS STARTED=====');
 
 app.get('/', function (req, res) {
 	res.sendfile(__dirname + '/index.html');
 });
 
-var frameInterval = setInterval(function(){
-	
-	//trigger frame advance
-	events.trigger("frame:advance");
-	
-	//look for collisions between objects
-	_.each(elementCollection.models, function(model, idx, collection) {
-		if(typeof model !== "undefined") { 	
-			CollisionDetection.detect(model, collection);
-		}
-	});		
-	
-	//look for collision with bounds
-	CollisionDetection.detect(boundsModel, elementCollection.models, {invert:true});
-	
-	//emit output to each socket
-	_.each(sockets, function(socket){
-		socket.emit('frame', elementCollection.toJSON());	
-	});
-
-}, FRAME_RATE)
-
-io.sockets.on('connection', function(socket) {
-
-	sockets.push(socket);
-
-	elementCollection.add(
-		new TankModel({
-			'id': socket.id,
-			'type': 'tank',
-			'x': _.random(0, boundsModel.get('x')),
-			'y': _.random(0, boundsModel.get('y')),
-			'a': _.random(0, 360)
-		}, {
-			'events': events
-		}
-	));
-
-	socket.on('move', function(move) {
-		try {
-			elementCollection.get(socket.id).set('move', move);
-		} catch(e) {}
-	});
-
-	socket.on('rotate', function(rotate) {
-		try {
-			elementCollection.get(socket.id).set('rotate', rotate);
-		} catch(e) {}
-	});
-
-	socket.on('shoot', function(shoot) {
-		try {
-			elementCollection.get(socket.id).shoot();
-		} catch(e) {}
-	});
-
-	socket.on('disconnect', function() {
-		elementCollection.remove(elementCollection.get(socket.id));
-	});
-
-});
+Game.start({'io': io});
