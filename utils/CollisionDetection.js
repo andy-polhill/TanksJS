9c56b549ae75d294de1abf6bc0329551c9919d1d
Andy <andy@thatguynamedandy.com>
2013-04-17 20:39:07 +0100

    Return true or false depending on whether a collision occurred

--- utils/CollisionDetection.js
+++ utils/CollisionDetection.js
@@ -9,8 +9,9 @@ define(function(require) {
 		//TODO: Implement radial collision detection!
 		detect : function(model, collection, opts) {
 		
-			var opts = (typeof opts === "undefined") ? {} : opts;
-		
+			var opts = (typeof opts === "undefined") ? {} : opts,
+				hasIntersect = false;
+			
 			var y = model.get('y'),
 				x = model.get('x'),
 				halfHeight = model.get('h') / 2,
@@ -23,6 +24,10 @@ define(function(require) {
 		
 			_.each(collection, function(candidate) {
 
+				var cid = candidate.get('id');
+				
+				if(id === cid) return; //don't compare the same object with itself
+				
 				var y = candidate.get('y'),
 					x = candidate.get('x'),
 					cHalfHeight = candidate.get('h') / 2,
@@ -30,28 +35,32 @@ define(function(require) {
 					cLeft = x - cHalfWidth,
 					cRight = x + cHalfWidth,
 					cTop = y - cHalfHeight,
-					cBottom = y + cHalfHeight,
-					cid = candidate.get('id');
+					cBottom = y + cHalfHeight;
 
-				/*console.log("left %d > cRight %d = ", left, cRight, (left > cRight));
-				console.log("cLeft %d > right %d = ", cLeft, right, (cLeft > right));
-				console.log("top %d > cBottom %d = ", top, cBottom, (top > cBottom));
-				console.log("cTop %d > bottom %d = ", cTop, bottom, (cTop > bottom));
-				console.log("id %d === cid %d", id, cid, (id === cid));
-				console.log(!(left > cRight || cLeft > right || top > cBottom || cTop > bottom || id === cid));*/
-
-				var outcome = !(left > cRight || cLeft > right || top > cBottom || cTop > bottom || id === cid);
+				var outcome = !(left > cRight || cLeft > right || top > cBottom || cTop > bottom);
 				
 				if((opts.invert === true && !outcome) || (opts.invert !== true && outcome)) {
-					if(_.isFunction(model.collide)) {
-						model.collide.call(model, candidate);
+					if(!hasIntersect) {
+						hasIntersect = true;
+					}				
+					if(_.isFunction(model[opts.callback])) {
+						model[opts.callback].call(model, candidate);
 					}
-					if(_.isFunction(candidate.collide)) {
-						candidate.collide.call(candidate, model);
+					if(_.isFunction(candidate[opts.callback])) {
+						candidate[opts.callback].call(candidate, model);
 					}
 				}
 			});
+			
+			return hasIntersect
 		}
 	}
-
 });
+
+
+/*console.log("left %d > cRight %d = ", left, cRight, (left > cRight));
+console.log("cLeft %d > right %d = ", cLeft, right, (cLeft > right));
+console.log("top %d > cBottom %d = ", top, cBottom, (top > cBottom));
+console.log("cTop %d > bottom %d = ", cTop, bottom, (cTop > bottom));
+console.log("id %d === cid %d", id, cid, (id === cid));
+console.log(!(left > cRight || cLeft > right || top > cBottom || cTop > bottom || id === cid));*/
