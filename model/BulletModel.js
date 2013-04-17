e440b2dd55e13a16db08cce54ff778d8b49dc265
Andy <andy@thatguynamedandy.com>
2013-04-17 20:37:20 +0100

    Added some comments

--- model/BulletModel.js
+++ model/BulletModel.js
@@ -7,8 +7,12 @@ define(function(require) {
 	var BulletModel = Backbone.Model.extend({
 
 		initialize: function( atts, opts ) {
+			
+			//listen to global frame advance
 			opts.events.on('frame:advance', this.frame, this);
 
+			//calculate horizontal and vertical velocity
+			//these will remane the same for the Bullet.
 			var radians = this.get('a') * (Math.PI / 180),
 				cos = Math.cos(radians),
 				sin = Math.sin(radians);
@@ -22,15 +26,22 @@ define(function(require) {
 			'x': 0, //horizontal
 			'y': 0, //vertical
 			'h': 3, //height
-			'w': 3 //width
+			'w': 3, //width
+			'type': 'bullet'
 		},
 		frame: function() {
+		
+			//calculate the new location
 			var left = this.get('x') - this.get('xv');
 			var top = this.get('y') - this.get('yv');
+			
+			//set it on the model
 			this.set('x', parseFloat((left).toFixed(2)));
 			this.set('y', parseFloat((top).toFixed(2)));
 		},
 		collide: function() {
+		
+			//bullet detonates no matter what it hits
 			this.unset('id');
 			this.destroy();
 		}
