d18f8b689fac176564af5d7d88d2cfd5d9ca0d58
Andy <andy@thatguynamedandy.com>
2013-04-19 09:56:30 +0100

    Removed switch statement in favour of more efficient lookup

--- client/js/app/view/GameView.js
+++ client/js/app/view/GameView.js
@@ -8,96 +8,85 @@ define([
 ], function(
 	$, _, Backbone, TankView, BarrierView, BulletView){
 
-  var GameView = Backbone.View.extend({
+	var ELEMENTS = {
+		'bullet' : BulletView,
+		'tank' : TankView,
+		'barrier' : BarrierView
+	};
+
+	var GameView = Backbone.View.extend({
   
-	events: {
-		"keyup" : "control",
-		"keydown" : "control"
-	},
-	initialize: function(opts) {
-		this.socket = opts.socket;
-		this.views = [];
-		this.$bounds = this.$el.find("#game");
-		this.collection.on('add', this.add, this);
-		this.collection.on('change', this.render, this);
-	},	
-	add: function(model) {	
-		var view;
-		switch(model.get('type')) {
-			case "tank": 
-				view = new TankView({
-					'model': model
-				});
-			break;
-			case "bullet": 
-				view = new BulletView({
-					'model': model
-				});
-			break;
-			case "barrier": 
-				view = new BarrierView({
-					'model': model,
-				})
-			break;
-		}
-		if(view) {
+		events: {
+			"keyup" : "control",
+			"keydown" : "control"
+		},
+		initialize: function(opts) {
+			this.socket = opts.socket;
+			this.views = [];
+			this.$bounds = this.$el.find("#game");
+			this.collection.on('add', this.add, this);
+			this.collection.on('change', this.render, this);
+		},	
+		add: function(model) {	
+			var view = new ELEMENTS[model.get('type')]({
+				'model': model
+			});
 			this.$bounds.append(view.$el);
-			this.views.push(view);		
-		}
-	},	
-	render: function() {
-		_.each(this.views, function(view){
-			view.render();
-		})
-	},
-	control: function( evt ) {
+			this.views.push(view);
+		},	
+		render: function() {
+			_.each(this.views, function(view){
+				view.render();
+			})
+		},
+		control: function( evt ) {
+			
+			var isKeydown = (evt.type === 'keydown') ? true : false;
 		
-		var isKeydown = (evt.type === 'keydown') ? true : false;
-
-		switch(evt.which) {
-			case 38: //up
-				if(isKeydown) {
-					this.socket.emit('move', '1');
-				} else { 
-					this.socket.emit('move', null);
-				}
-				evt.preventDefault();
-				break;
-			case 40: //down
-				if(isKeydown) {
-					this.socket.emit('move', '-1');
-				} else {
-					this.socket.emit('move', null);
-				}
-				evt.preventDefault();
-				break;
-			case 37: //rotate left
-				if(isKeydown) {
-					this.socket.emit('rotate', '-1');
-				} else {
-					this.socket.emit('rotate', null);
-				}
-				evt.preventDefault();
-				break;
-			case 39: //rotate right
-				if(isKeydown) {
-					this.socket.emit('rotate', '1');
-				} else {
-					this.socket.emit('rotate', null);
-				}
-				evt.preventDefault();
-				break;
-			case 32: //spacebar
-				if(!isKeydown) {
-					this.socket.emit('shoot', true);
-				}
-				evt.preventDefault();
-				break;
+			switch(evt.which) {
+				case 38: //up
+					if(isKeydown) {
+						this.socket.emit('move', '1');
+					} else { 
+						this.socket.emit('move', null);
+					}
+					evt.preventDefault();
+					break;
+				case 40: //down
+					if(isKeydown) {
+						this.socket.emit('move', '-1');
+					} else {
+						this.socket.emit('move', null);
+					}
+					evt.preventDefault();
+					break;
+				case 37: //rotate left
+					if(isKeydown) {
+						this.socket.emit('rotate', '-1');
+					} else {
+						this.socket.emit('rotate', null);
+					}
+					evt.preventDefault();
+					break;
+				case 39: //rotate right
+					if(isKeydown) {
+						this.socket.emit('rotate', '1');
+					} else {
+						this.socket.emit('rotate', null);
+					}
+					evt.preventDefault();
+					break;
+				case 32: //spacebar
+					if(!isKeydown) {
+						this.socket.emit('shoot', true);
+					}
+					evt.preventDefault();
+					break;
+			}
 		}
-	}
 
-  });
+	});
   
-  return GameView;
+	return GameView;
 
 });
