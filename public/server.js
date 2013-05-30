7dba183b56fef8e4de1eb1dd51654e0e55e5fb32
Andy <andy@thatguynamedandy.com>
2013-05-30 16:25:55 +0100

    Put server.js back in the correct place

--- public/server.js
+++ public/server.js
@@ -3,31 +3,31 @@ var requirejs = require('requirejs')
 ,	http = require('http')
 ,	app = express()
 ,	server = http.createServer(app)
-,	io = require('socket.io').listen(server)
+,	io = require('socket.io').listen(server, {'flash policy port': -1 })
 ,	static;
 
+//AppFog Environment
 io.configure('production', function(){
 	console.log("production sockets config");
-	io.set('transports', ['htmlfile', 'xhr-polling', 'jsonp-polling']);
+	io.set('transports', ['flashsocket', 'htmlfile', 'xhr-polling', 'jsonp-polling']);
 	io.enable('browser client minification');  // send minified client
 	io.enable('browser client etag');          // apply etag caching logic based on version number
 	io.enable('browser client gzip');          // gzip the file
+	io.disable('flash policy server');          // no websockets, so try flashsocket
 	io.set('log level', 1);                    // reduce logging
-	static = 'target';
 });
 
 io.configure('development', function(){
 	console.log("development sockets config");
 	io.set('log level', 2);
-	static = 'public';
 });
 
 requirejs.config({
-    baseUrl: __dirname + '/' + static + '/js',
+    baseUrl: __dirname + '/js',
     nodeRequire: require
 });
 
-app.use(express.static(static));
+app.use(express.static(__dirname));
 
 server.listen(80);
 
