4f02369fd0f4e7579843118b7a2f5bf786c2784b
Andy <andy@thatguynamedandy.com>
2013-06-13 16:20:49 +0100

    bootstrap

--- public/js/client.js
+++ public/js/client.js
@@ -1,15 +1,19 @@
 require.config({
 	baseUrl: "/js",
 	paths: {
+        'bootstrap': ['//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.1/js/bootstrap.min', 'libs/bootstrap-min'],
 		jquery: "lib/jquery",
 		underscore: "lib/underscore",
 		backbone: "lib/backbone",
 		text: "lib/text",
 		template: "../template"
-	}
+	},
+	shim: {
+        "bootstrap": ["jquery"]
+    }
 });
 
-require(['router/router'], function(AppRouter){
+require(['router/router', 'jquery', 'bootstrap'], function(AppRouter, $){
 
 	//globally scoped event aggregator
 	Vents = _.extend({}, Backbone.Events);
@@ -21,7 +25,7 @@ require(['router/router'], function(AppRouter){
 	});
 
 	Vents.bind("select:tank", function( args ){
-		router.navigate(document.location.hash+ "/" + args.variant, {trigger: true});
+		router.navigate(document.location.hash + "/" + args.variant, {trigger: true});
 	});
 
 	Vents.bind("game:over", function( args ){
